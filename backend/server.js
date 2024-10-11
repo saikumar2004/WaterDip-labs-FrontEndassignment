84d879b785917c077d718c1802625178613dd90b
SAI KUMAR@LAPTOP-52G4EK29 <saikumarkaicherla2004@gmail.com>
2024-10-11 16:51:21 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const csv = require('csv-parser');
+const app = express();
+
+app.use(cors());
+
+
+let bookingsData = [];
+
+
+fs.createReadStream('hotel_booking.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    bookingsData.push(row); 
+    console.log(row);
+  })
+  .on('end', () => {
+    console.log('file successfully loading');
+  });
+  
+
+app.get('/api/bookings', (req, res) => {
+  const { startDate, endDate } = req.query;
+
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+
+  const filteredData = bookingsData.filter((booking) => {
+    const arrivalDate = new Date(`${booking.arrival_date_year}-${booking.arrival_date_month}-${booking.arrival_date_day_of_month}`);
+    return arrivalDate >= start && arrivalDate <= end;
+  });
+ 
+  res.json(filteredData);
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ front-end/src/App.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ApexCharts from "react-apexcharts";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const App: React.FC = () => {
+ 
+};
+
+export default App;
